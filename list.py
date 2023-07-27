3100c46d6076a5f28b3ab689ee39283575c5b8ea
Nitish Mishra <105452945+nitish0307@users.noreply.github.com>
2023-07-27 15:05:49 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,20 @@
+thislist = ["grapes", "tomato", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "guava", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+thislist = ["mango", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
